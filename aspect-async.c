071a96f471a7bec8dcd3aef0ca148340fed97d69
Kyle Metscher <kscott.mets@gmail.com>
2022-10-06 03:30:26 -0700

    Working DFT, callback blocks on full buffer

--- aspect-async.c
+++ aspect-async.c
@@ -4,21 +4,24 @@
 #include <complex.h>
 #include <fftw3.h>
 #include <signal.h>
+#include <complex.h>
 
 #define FORMAT PA_SAMPLE_S16LE
 #define SAMPLERATE 48000
+// FIXME: set format and sampling based on chosen sink / pa params
 #define CHANNELS 1
-#define BUFSIZE 1024
+#define BUFSIZE 128
+#define PEAK 32767.0
 
 static pa_context *context = NULL;
 static pa_stream *stream = NULL;
 static pa_mainloop_api *loop_api = NULL;
-static pa_sample_spec sample_spec = {
-    .format = FORMAT, 
-    .rate = SAMPLERATE, 
-    .channels = CHANNELS
-};
+static pa_sample_spec sample_spec = {.format = FORMAT, .rate = SAMPLERATE, .channels = CHANNELS};
 static pa_channel_map channel_map;
+static double complex in[BUFSIZE];
+static double complex out[BUFSIZE];
+static int buf_index = 0;
+static fftw_plan plan;
 
 static void stream_state_cb(pa_stream *stream, void *data) {
     const pa_stream_state_t STATE = pa_stream_get_state(stream);
@@ -50,7 +53,21 @@ static void stream_read_cb(pa_stream *stream, size_t bytes, void *user_data) {
             printf("Buffer seemingly empty\n");
             return;
         }
-        printf("%d\n", *stream_data);
+        if (buf_index == BUFSIZE) {
+            printf("Input buffer full, executing DFT\n");
+            fftw_execute(plan);
+            for (unsigned int i = 0; i < BUFSIZE; i++) {
+                printf("%f+%fi at output array index %d", 
+                    creal(out[i]), cimag(out[i]), i);
+            }
+            buf_index = 0;
+        }
+        printf("Signed int real sample: %d\n", *stream_data);
+        double complex complex_sample = CMPLX((*stream_data / PEAK), 0);
+        in[buf_index] = complex_sample;
+        printf("Complex double at index %d of input array: %f+%fi\n", 
+            buf_index, creal(complex_sample), cimag(complex_sample));
+        buf_index++;
         if (pa_stream_drop(stream) != 0) {
             fprintf(stderr, "Dropping after peek failed\n");
         }
@@ -80,6 +97,9 @@ static void context_state_cb(pa_context *context, void *main_loop) {
 }
 
 int main(int argc, char argv[]) {
+    // Initialize DFT plan
+    plan = fftw_plan_dft_1d(BUFSIZE, in, out, FFTW_FORWARD, FFTW_MEASURE);
+
     // Get the main loop
     pa_mainloop *main_loop = pa_mainloop_new();
     printf("Made loop\n");
@@ -101,6 +121,8 @@ int main(int argc, char argv[]) {
     // Iterate forever
     pa_mainloop_run(main_loop, NULL);
     printf("After loop, tearing down\n");
+
+    fftw_destroy_plan(plan);
 
     pa_context_disconnect(context);
     pa_context_unref(context);
